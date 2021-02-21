c10ffb215bf1570dda79adce85ccc24e04740158
Bharat Nilam <bharatnilam62@gmail.com>
2021-02-21 23:48:34 +0530

    move data collection to /

---
+++ data_collection.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 14 14:54:50 2021
+
+@author: bharatnilam
+"""
+
+from scipy.io import arff
+import pandas as pd
+
+data1 = arff.loadarff('maxwell.arff')
+mw = pd.DataFrame(data1[0])
+
+data2 = arff.loadarff('desharnais.arff')
+ds = pd.DataFrame(data2[0])
+
+mw.to_csv('maxwell.csv', index = False)
+
+ds.to_csv('desharnais.csv', index = False)
